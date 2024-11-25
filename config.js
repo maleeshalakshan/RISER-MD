c5bb6b0a661457712ef25b50a271f4326d5ef9f5
BotMvTvFilm <pasidhukaveesha@gmail.com>
2024-11-25 19:25:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID : process.env.SESSION_ID | | undefined ? 'rWRDRSIT#sKoxRIDrH7lSSvLgpbXoE0XVajQnd7Oso7XRMrwsMvU' : process.env.SESSION_ID,
+SESSION_ID : process.env.SESSION_ID | | undefined ? 'Put Your Season Id' : process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/HgnkM7X/a14be8c0c96c8052.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!
 };
