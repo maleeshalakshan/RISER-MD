dd946f467ec7dc9f18b0863758f32bf292749229
Maleesha Lakshan <109362145+maleeshalakshan@users.noreply.github.com>
2024-12-12 01:26:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID : process.env.SESSION_ID || "Put Seasion Id Here" : process.env.SESSION_ID,
+SESSION_ID : process.env.SESSION_ID || "crVXTZYR#bUpTiKRtcL3ZVMSbqesBl_bLkbJaSzyuK3YOPmU0Mkg" : process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/HgnkM7X/a14be8c0c96c8052.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!",
 };
